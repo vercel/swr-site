e344e8df7cae03e8b8aeded8be757848f88b2c2f
Shu Ding <g@shud.in>
2020-10-05 02:31:24 +0800

    use nextra

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
-import '.nextra/styles.css'
-import GoogleFonts from 'next-google-fonts'
+import 'nextra-theme-docs/style.css'
+import '../styles.css'
 
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
-      <GoogleFonts
-        href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
-        rel="stylesheet"
-      />
       <Component {...pageProps} />
     </>
   )
