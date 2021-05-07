8f285a38790530051dc3921cbc8249879d43acb6
Markoz Pena <markdrew53@gmail.com>
2021-05-07 09:56:08 -0500

    feat: added support for Spanish language

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withNextra = require('nextra')({
 
 module.exports = withNextra({
   i18n: {
-    locales: ['en-US', 'zh-CN'],
+    locales: ['en-US', 'zh-CN', 'es-ES'],
     defaultLocale: 'en-US'
   }
 })
