b36c102ebb4ef8778fbecece48ebb02327d929ec
uttk <uttk8128@gmail.com>
2021-05-16 06:59:38 +0900

    doc: translate the swr title into Japanese (#71)

--- components/features.js
+++ components/features.js
@@ -11,7 +11,8 @@ const Feature = ({ text, icon }) => (
 
 const TITLE_WITH_TRANSLATIONS = {
   'en-US': 'React Hooks library for data fetching',
-  'zh-CN': '用于数据请求的 React Hooks 库'
+  'zh-CN': '用于数据请求的 React Hooks 库',
+  'ja': "データ取得のための React Hooks ライブラリ"
 }
 
 export default () => {
