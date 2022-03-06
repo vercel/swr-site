504bb777a287f8ce188458211fdf764d103221d8
Shu Ding <g@shud.in>
2022-03-06 19:01:32 +0100

    add blog index page; fix blog 404

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.js",
-  unstable_stork: false,
-  unstable_contentDump: true,
+  unstable_flexsearch: true,
   unstable_staticImage: true,
 });
 
@@ -42,11 +41,6 @@ module.exports = withNextra({
         source: "/blog/swr-1",
         destination: "/blog/swr-v1",
         statusCode: 301,
-      },
-      {
-        source: "/blog",
-        destination: "/blog/swr-v1",
-        statusCode: 302,
       },
       {
         source: "/docs",
