10a5bbb072831a8f14b007f5522ef032141bb255
Guilherme Sousa <guilherssousa@gmail.com>
2022-06-16 21:43:42 -0300

    add meta.pt-BR, index.pt-BR

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withNextra = require("nextra")({
 
 module.exports = withNextra({
   i18n: {
-    locales: ["en-US", "zh-CN", "es-ES", "ja", "ko", "ru"],
+    locales: ["en-US", "zh-CN", "es-ES", "pt-BR", "ja", "ko", "ru"],
     defaultLocale: "en-US",
   },
   redirects: () => {
